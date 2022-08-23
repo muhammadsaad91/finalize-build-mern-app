200ba7983237026885ad94b7653f18368536274d
muhammadsaad91 <saadattari91@gmail.com>
2022-08-23 19:19:33 +0500

    deploy app

--- app.js
+++ app.js
@@ -39,6 +39,10 @@ app.get('/signup', (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  }
+  );
 }
 
 
