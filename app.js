cc75f60418604c912652ff70f8cf06b79d46fd81
muhammadsaad91 <saadattari91@gmail.com>
2022-08-23 20:07:53 +0500

    make it better

--- app.js
+++ app.js
@@ -36,10 +36,12 @@ app.use(require('./router/auth'));
 // )
 
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-}
-
+if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
+ }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
